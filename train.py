c7b16079bdeaa44a52ae346c4f54ffdd2bdd13ab
Justin Rossiter <justin.rossiter@ufl.edu>
2022-12-01 23:19:05 -0500

    added bounding box generation

--- train.py
+++ train.py
@@ -6,17 +6,36 @@ import os
 from sklearn.model_selection import train_test_split
 from PIL import Image
 import shutil
+from skimage.filters import threshold_otsu
+from skimage.morphology import dilation, disk, erosion
 
-preceding_path = "/blue/eel5840/justin.rossiter"
+preceding_path = "/blue/eel5840/justin.rossiter/final_project_team_square_root"
 
 plt.ion()   # interactive mode
 #https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
 
 def generateImageAndLabel(split, i, idx):
-    image = Image.fromarray(dataset[:, :, idx], mode = 'L').resize((320, 320))
+    image_grayscale = dataset[:, :, idx]
+    image_dilated = dilation(image_grayscale) #Dilation
+    image_erosion = erosion(image_dilated) #Image Erosion
+    radius = 15 #Otsu thresholding
+    footprint = disk(radius)
+    local_otsu = rank.otsu(image_erosion, footprint)
+    image_local_otsu_threshold = image_erosion >= local_otsu
+    image_bounding = 1-image_local_otsu_threshold
+    left_edge = np.where(np.array([np.sum(image_bounding[:, i]) for i in range(300)]) > 0)[0]
+    right_edge = np.where(np.array([np.sum(image_bounding[:, i]) for i in range(300)]) > 0)[-1]
+    top_edge = np.where(np.array([np.sum(image_bounding[i, :]) for i in range(300)]) > 0)[0]
+    bottom_edge = np.where(np.array([np.sum(image_bounding[i, :]) for i in range(300)]) > 0)[-1]
+    width = (right_edge - left_edge)/300.0
+    height = (bottom_edge - top_edge)/300.0
+    x_mu = (right_edge + left_edge)/600.0
+    y_mu = (bottom_edge + top_edge)/600.0
+    print(image_local_otsu_threshold)
+    image = Image.fromarray(image_grayscale, mode = 'L').resize((320, 320))
     image.save(os.path.join(preceding_path, split, "images", str(i) + "_" + str(idx) + ".jpg"))
     with open(os.path.join(preceding_path, split, "labels", str(i) + "_" + str(idx) + ".txt"), "w") as f:
-        f.write(str(i) + " 0.5 0.5 1 1")
+        f.write(str(i) + " " + str(x_mu) + " " + str(y_mu) + " " + str(width) + " " + str(height))
 
 if __name__ == "__main__":
 
